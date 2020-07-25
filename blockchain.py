77bcfbd61fb34c45c4edda7f11f5e99d380fde60
piyushxbajaj <piyushpiyush41@gmail.com>
2020-07-25 13:56:02 +0530

    initial

---
+++ blockchain.py
@@ -0,0 +1,25 @@
+class Blockchain(object):
+    def __init(self):
+        self.chain = []
+        self.current_transaction = []
+
+    def new_block(self):
+        # for adding new block to the chain
+        pass
+    def new_transaction(self):
+        # Adding a new transaction to the list of transactions
+        pass
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        pass
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        pass
+
+
+
+
+
+
